2dc4fa81ffd01d8e3f979c52bffa3e30e76901ca
Guigo-737 <rodrigohernandes@colegiopaulodetarso.com.br>
2025-05-26 16:21:04 +0000

    a

---
+++ ex1.js
@@ -0,0 +1,12 @@
+let nome = Guigo
+let idade = 14;
+let ano = 365;
+let hora = 24;
+let minuto = 60;
+let segundo = 60;
+let dias = idade * ano;
+let minutos = dias * hora;
+console.log("Olá," + nome)
+console.log("dias vivo" + dias)
+console.log("minutos vivo" + minutos)
+console.log()
